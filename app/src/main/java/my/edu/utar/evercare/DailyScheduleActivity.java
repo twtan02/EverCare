d9fce0e901a143e4e143e19a37815d851e7f304f
twtan02 <tanteckwei73@1utar.my>
2023-07-16 22:26:38 +0800

    Fourth commit

---
+++ app/src/main/java/my/edu/utar/evercare/DailyScheduleActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utar.evercare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_schedule);
+    }
+}
---
+++ app/src/main/java/my/edu/utar/evercare/FontSizeActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utar.evercare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FontSizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_font_size);
+    }
+}
